0fb1695cbb34f1425713c72050fa51e72033c9b7
JVSRamos <joaoramosj@usp.br>
2023-12-05 20:59:28 -0300

    feat: add game observer to view
    co-authored-by: Vitão <vernalhav@gmail.com>

--- website/js/view.js
+++ website/js/view.js
@@ -1,5 +1,28 @@
-const guessInput = /** @type {HTMLInputElement} */ (document.getElementById("guess"))
+const guessInput = /** @type {HTMLInputElement} */ (
+    document.getElementById("guess")
+)
 
 function processGuessInput() {
     guess(guessInput.value)
 }
+
+/**
+ * @param {GameState} newState
+ * @param {GameInfo} info
+ */
+function gameObserver(newState, info) {
+    if (newState === "error") {
+        showError(info)
+    }
+}
+
+/** @param {GameInfo} info */
+function showError(info) {
+    window.alert(info)
+}
+
+function setup() {
+    gameObservers.push(gameObserver)
+}
+
+window.addEventListener("load", setup)
