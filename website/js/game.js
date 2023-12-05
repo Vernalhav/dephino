8493ea331b73e38bd75e961ad2349aa1a5679ab6
Victor Giovannoni <vernalhav@usp.br>
2023-12-04 22:40:52 -0300

    feat: add basic word guess flow
    co-authored-by: Jão <joaovsramosj@gmail.com>

--- website/js/game.js
+++ website/js/game.js
@@ -1,11 +1,25 @@
 window.onload = main
 
+/** @type {string[]} */
 let words = []
+/** @type {DictionaryEntry?} */
+let wordDefinition = null
+
+const guessInput = /** @type {HTMLInputElement} */ (document.getElementById("guess"))
+
+function guess() {
+    const guess = guessInput.value
+    console.log(guess)
+    console.log(guess.toLowerCase() === wordDefinition.word.toLowerCase())
+}
 
 async function newWord() {
+    const word = choice(words)
+    wordDefinition = await getDefinition(word)
+    console.log(word, wordDefinition)
 }
 
 async function main() {
     words = await getWords()
-    console.log(words)
+    newWord()
 }
